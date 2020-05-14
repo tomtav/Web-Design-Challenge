052ee2f6db8f4fdd176e9acaecf52ceab616a6de
tomtav <tt2377@gmail.com>
2020-05-14 18:31:19 -0400

    Initial project structure and code

---
+++ WebVisualizations/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', _ => {
+    let sideNavOptions = {
+        'closeOnClick': true,
+    }
+    let sidenav = document.querySelectorAll('.sidenav');
+    let instancesSideNav = M.Sidenav.init(sidenav, sideNavOptions);
+
+    let collapseOptions = {
+        'closeOnClick': true,
+    }
+    let collapsible = document.querySelectorAll('.collapsible');
+    let instancesCollapsible = M.Collapsible.init(collapsible, collapseOptions);
+
+    let dropdownOptions = {
+        'constrainWidth': false,
+        'coverTrigger': false
+    }
+    let dropdown = document.querySelectorAll('.dropdown-trigger');
+    let instanceDropDown = M.Dropdown.init(dropdown, dropdownOptions);
+});
